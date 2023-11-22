41bd28f14d492f6eba17b359375c7c974de37a3a
Aman07a <aman16.aa17@gmail.com>
2023-11-22 08:44:15 +0100

    Creating the view for the shortcode (part 2)

--- views/mv-translations_shortcode.php
+++ views/mv-translations_shortcode.php
@@ -1,3 +1,28 @@
+<?php
+if (isset($_POST['submitted'])) {
+    $title = $_POST['mv_translations_title'];
+    $content = $_POST['mv_translations_content'];
+    $singer = $_POST['mv_translations_singer'];
+    $transliteration = $_POST['mv_translations_transliteration'];
+    $video = $_POST['mv_translations_video_url'];
+
+    $post_info = [
+        'post_type' => 'mv-translations',
+        'post_title' => $title,
+        'post_content' => $content,
+        'tax_input' => [
+            'singers' => $singer,
+        ],
+        'post_status' => 'pending',
+    ];
+
+    $post_id = wp_insert_post($post_info);
+
+    global $post;
+    $mv_translations_post_type = new MV_Translations_Post_Type(MV_TRANSLATIONS_PATH);
+    $mv_translations_post_type->save_post($post_id, $post);
+}
+?>
 <div class="mv-translations">
     <form action="" method="POST" id="translations-form">
         <h2><?php esc_html_e('Submit new translation', 'mv-translations'); ?></h2>
